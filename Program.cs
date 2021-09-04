87d30a4530ec3a9eae5b96993f10e1fc029e9260
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-05 02:14:55 +0900

    Paralleの基本的な使い方を実装してみた

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class Program
+    {
+        private static int N = 1000;
+        
+        static void Main(string[] args)
+        {
+            // using a named method
+            Parallel.For(0, N, Method2);
+
+            // using an anonymous method
+            Parallel.For(0, N, delegate(int i)
+            {
+                Console.WriteLine($"delegate: {i}");
+            });
+
+            // using a lambda expression
+            Parallel.For(0, N, i =>
+            {
+                Console.WriteLine($"lambda: {i}");
+            });
+        }
+
+        private static void Method2(int i)
+        {
+            Console.WriteLine($"Method2: {i}");
+        }
+    }
+}
